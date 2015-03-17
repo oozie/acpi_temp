c3d9e50bd64dd7c1631e6f4332181ad7079b0125
Slawek Ligus <root@ooz.ie>
2015-03-17 13:35:30 -0700

    Turning acpi_temp into a continuous detector.

--- source/acpi_temp.d
+++ source/acpi_temp.d
@@ -3,23 +3,25 @@ import std.array;
 import std.datetime;
 import std.process;
 import std.stdio;
+import core.thread;
 
 immutable METRIC_NAME = "acpi.temperature";
 
 void main()
 {
-    auto acpi_temp = pipeProcess(["/usr/bin/acpi", "-t"], Redirect.stdout);
-    scope(exit) wait(acpi_temp.pid);
-    
-    long time_now = Clock.currTime().toUnixTime();
-
-    foreach (line; acpi_temp.stdout.byLine) {
-        char[][] unit_status = line.split(": "); 
-        string unit = cast(string)unit_status[0].replace(" ", "");
-        string status = cast(string)unit_status[1];
-        auto status_array = status.split(" ");
-        string value = status_array[1];
+    while(true) {
+        auto acpi_temp = pipeProcess(["/usr/bin/acpi", "-t"], Redirect.stdout);
+        scope(exit) wait(acpi_temp.pid);
         
-        writeln(METRIC_NAME, " ", time_now, " ", value, " unit=", unit);
+        long time_now = Clock.currTime().toUnixTime();
+        foreach (line; acpi_temp.stdout.byLine) {
+            char[][] unit_status = line.split(": "); 
+            string unit = cast(string)unit_status[0].replace(" ", "");
+            string status = cast(string)unit_status[1];
+            auto status_array = status.split(" ");
+            string value = status_array[1];
+            writeln(METRIC_NAME, " ", time_now, " ", value, " unit=", unit);
+        }
+        Thread.sleep(dur!("seconds")(60));
     }
 }
